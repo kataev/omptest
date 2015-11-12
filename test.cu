5d993bc8e33363fa4cbf1bb98217e5197a4b3f3a
Denis Kataev <denis.a.kataev@gmail.com>
2015-11-12 23:21:20 +0500

    Cuda work

--- test.cu
+++ test.cu
@@ -1,22 +1,7 @@
 #include <device_launch_parameters.h>
-__global__ void matMult(float * a, float * b, int n, float * c) {
-    int BLOCK_SIZE = 512;
 
-    int bx = blockIdx.x;
-    int by = blockIdx.y;
-
-    int tx = threadIdx.x;
-    int ty = threadIdx.y;
-
-    float sum = 0.0f;
-
-    int ia = n * BLOCK_SIZE * by + n * ty;
-    int ib = n * BLOCK_SIZE * bx * tx;
-    int k;
-    for (k =0; k < n;k++)
-        sum += a[ia+k ] * b[ib + k*n];
-    int ic = n * BLOCK_SIZE * by + BLOCK_SIZE * bx;
-
-
-    c[ic + n * ty +tx] = sum;
+__global__ void add(int *a, int *b, int *c ) {
+    int tid = blockIdx.x; // handle the data at this index
+    if (tid < N)
+        c[tid] = a[tid] + b[tid];
 }
